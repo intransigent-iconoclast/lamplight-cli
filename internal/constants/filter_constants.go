c6d687ec4443224550e17fa4c939d028ab13d657
Benjamin-Pasternak <bp443@scarletmail.rutgers.edu>
2025-12-09 21:59:58 -0700

    adding search + basic filtering

---
+++ internal/constants/filter_constants.go
@@ -0,0 +1,12 @@
+package constants
+
+// torznab categories from jackett ... not sure if prowlar is different
+var BookCategories = []int{
+	7000, // Books
+	7010, // Books/Mags
+	7020, // Books/EBook
+	7030, // Books/Comics
+	7040, // Books/Technical
+	7050, // Books/Other
+	7060, // Books/Foreign
+}
